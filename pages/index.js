798c563987066b47c7b344b72d9bd82716747474
gregoryandre <60001263+gregoryandre@users.noreply.github.com>
2022-01-28 12:07:53 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 function Home(){
         return (
             <div>
-                <h1>Home</h1>
+                <h1>Home - André Gregory</h1>
                 <Contador/>
             </div>
         )
